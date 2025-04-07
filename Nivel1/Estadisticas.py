52e720f56716f3864fa52adbb96167c48b4ced2f
dinovillalobos <villalobosdoricardo@gmail.com>
2025-04-06 18:16:30 -0600

    agregando comentarios

--- Nivel1/Estadisticas.py
+++ Nivel1/Estadisticas.py
@@ -1,29 +1,3 @@
-"""🧱 Nivel 1 - Día 1: Código Legible y Ordenado (Python o C#)
-🎯 Objetivo:
-Escribir código claro, con nombres de variables descriptivos, buena indentación, y comentarios que expliquen el por qué, no el cómo.
-
-🔍 Desafío:
-Crea un programa que reciba una lista de calificaciones de alumnos (entre 0 y 100), y devuelva:
-
-El promedio.
-
-La calificación más alta.
-
-La calificación más baja.
-
-👉 Usa buenas prácticas:
-
-Nombres de variables claros.
-
-Comentarios donde haga falta.
-
-Código bien indentado.
-
-Nada de cosas como a = 10 si no se sabe qué es a.
-
-📝 Extra (opcional):
-Si terminas rápido, agrega una opción para ingresar las calificaciones desde consola, separadas por comas."""
-
 
 # Programa para calcular estadísticas de calificaciones
 
@@ -32,14 +6,12 @@ def obtener_estadisticas(calificaciones):
     Recibe una lista de calificaciones (números entre 0 y 100)
     y devuelve el promedio, la calificación más alta y la más baja.
     """
-    # Aquí irá la lógica
-
-    #validar calificaciones
+    #filtramos las calificaciones 
     calificaciones_validadas = [c for c in calificaciones if 0 <= c <= 100 ]
-
+    #evitamos que se dividan en cero 
     if not calificaciones_validadas:
         return 0,0,0
-
+    #calcular el promedio para obtener la calificacion 
     promedio = sum(calificaciones_validadas) / len(calificaciones_validadas)
 
     minimo = min(calificaciones_validadas)
